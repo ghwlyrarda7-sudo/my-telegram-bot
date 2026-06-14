6fc625c240e0d46d23a6f573468854ba824faf8a
ghwlyrarda7-sudo <ghwlyrarda7@gmail.com>
2026-06-15 00:52:49 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,11 @@
 import telebot
 import os
 
-# Get the token from Railway environment variables
+# Get the API token from Railway environment variables
 API_TOKEN = os.environ.get('API_TOKEN')
 bot = telebot.TeleBot(API_TOKEN)
 
-# Handle the /start command
+# Handle /start command
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, "البوت يعمل الآن بنجاح!")
