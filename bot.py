49b1119e790e7fef771ffc561558a308afc1ed23
ghwlyrarda7-sudo <ghwlyrarda7@gmail.com>
2026-06-15 00:26:48 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,14 @@
 import telebot
 import os
 
-# جلب التوكن من المتغيرات التي قمت بضبطها في Railway
+# جلب التوكن من المتغيرات في Railway
 API_TOKEN = os.environ.get('API_TOKEN')
 bot = telebot.TeleBot(API_TOKEN)
 
 # معالج الأمر /start
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "أهلاً بك! البوت يعمل الآن بنجاح.")
+    bot.reply_to(message, "البوت يعمل الآن بنجاح.")
 
 print("البوت يعمل الآن وينتظر الرسائل...")
 
